7e303049609cd4959cbb85da3d11ec9eb9f292af
unknown <subo0521@hanmail.net>
2022-07-15 14:57:49 +0900

    feat: firebase DB 연동

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 // for Home import
 const Home = () => {
+    const [tweet, setTweet] = useState("");
+    
+    const onSubmit = (event) => {
+        // 아무것도 입력하지 않는 행위 방지
+        event.preventDefault();
+    };
+
+    const onChange = (event) => {
+        const {
+            target: { value },           
+        } = event;
+        setTweet(value);
+    };
+
     return (
         <div>
-        <form>
+        <form onSubmit={onSubmit}>
             <input
+                value={tweet}
+                onChange={onChange}
                 type="text"
                 placeholder="what's on your mind?"
                 maxLength={120}
